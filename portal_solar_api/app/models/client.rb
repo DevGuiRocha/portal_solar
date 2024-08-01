922099d8228ffca1cdea5759a171710c7a9890a9
João Guilherme <jguilherme.1997@gmail.com>
2024-07-31 22:18:52 -0300

    Adição da gem bcrypt juntamente com remoção da regra das senhas

--- portal_solar_api/app/models/client.rb
+++ portal_solar_api/app/models/client.rb
@@ -3,5 +3,5 @@ class Client < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true
 end
